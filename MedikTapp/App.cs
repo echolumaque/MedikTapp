08a66f5ab0de1c90c10da12f723592a429a4137a
Jhericoh Janquill Lumaque <echo_lumaque@mobitech.dev>
2022-11-17 22:15:37 +0800

    Inseted curly braces

--- MedikTapp/App.cs
+++ MedikTapp/App.cs
@@ -30,122 +30,123 @@ using Preferences = MedikTapp.Constants.Preferences;
 [assembly: ExportFont("Font Awesome 6.1.1 Pro-Solid-900.otf", Alias = "fas")]
 [assembly: ExportFont("Font Awesome 6.1.1 Pro-Thin-100.otf", Alias = "fat")]
 
-namespace MedikTapp;
-
-public partial class App : Application
+namespace MedikTapp
 {
-    private readonly AppConfigService _appConfigService;
-    private readonly IConnectivity _connectivity;
-    private readonly GraphicsService _graphicsService;
-    private readonly InitializeDataService _initializeDataService;
-    private readonly IMainThread _mainThread;
-    private readonly MedikTappService _medikTappService;
-    private readonly NavigationService _navigationService;
-    private readonly NotificationService _notificationService;
-    private readonly IToast _toast;
-
-    public App(AppConfigService appConfigService,
-        IConnectivity connectivity,
-        GraphicsService graphicsService,
-        InitializeDataService initializeDataService,
-        IMainThread mainThread,
-        MedikTappService medikTappService,
-        NavigationService navigationService,
-        NotificationService notificationService,
-        IToast toast,
-        IPreferences preferences)
+    public partial class App : Application
     {
-        _appConfigService = appConfigService;
-        _connectivity = connectivity;
-        _graphicsService = graphicsService;
-        _medikTappService = medikTappService;
-        _initializeDataService = initializeDataService;
-        _mainThread = mainThread;
-        _navigationService = navigationService;
-        _notificationService = notificationService;
-        _toast = toast;
+        private readonly AppConfigService _appConfigService;
+        private readonly IConnectivity _connectivity;
+        private readonly GraphicsService _graphicsService;
+        private readonly InitializeDataService _initializeDataService;
+        private readonly IMainThread _mainThread;
+        private readonly MedikTappService _medikTappService;
+        private readonly NavigationService _navigationService;
+        private readonly NotificationService _notificationService;
+        private readonly IToast _toast;
 
-        SyncfusionLicenseProvider.RegisterLicense("NjQzMTA4QDMyMzAyZTMxMmUzMGdCUTc5N2ZmN21lckRHVXp2YzdranZ2V0FGTHVKeVFSa1pVSlBCaVpWL2M9");
-        Resources.Add(new LightTheme());
-        VersionTracking.Track();
-
-        if (VersionTracking.IsFirstLaunchEver)
-            navigationService.SetRootPage<OnboardingPage>();
-        else
+        public App(AppConfigService appConfigService,
+            IConnectivity connectivity,
+            GraphicsService graphicsService,
+            InitializeDataService initializeDataService,
+            IMainThread mainThread,
+            MedikTappService medikTappService,
+            NavigationService navigationService,
+            NotificationService notificationService,
+            IToast toast,
+            IPreferences preferences)
         {
-            if (preferences.Get(Preferences.HasLoggedAccount, false))
-                navigationService.SetRootPage<MainPage>(isOffWhitePageBgColor: true);
+            _appConfigService = appConfigService;
+            _connectivity = connectivity;
+            _graphicsService = graphicsService;
+            _medikTappService = medikTappService;
+            _initializeDataService = initializeDataService;
+            _mainThread = mainThread;
+            _navigationService = navigationService;
+            _notificationService = notificationService;
+            _toast = toast;
+
+            SyncfusionLicenseProvider.RegisterLicense("NjQzMTA4QDMyMzAyZTMxMmUzMGdCUTc5N2ZmN21lckRHVXp2YzdranZ2V0FGTHVKeVFSa1pVSlBCaVpWL2M9");
+            Resources.Add(new LightTheme());
+            VersionTracking.Track();
+
+            if (VersionTracking.IsFirstLaunchEver)
+                navigationService.SetRootPage<OnboardingPage>();
             else
-                navigationService.SetRootPage<AccountPage>(isOffWhitePageBgColor: true);
+            {
+                if (preferences.Get(Preferences.HasLoggedAccount, false))
+                    navigationService.SetRootPage<MainPage>(isOffWhitePageBgColor: true);
+                else
+                    navigationService.SetRootPage<AccountPage>(isOffWhitePageBgColor: true);
+            }
+
+            NotificationCenter.Current.NotificationTapped += GotoAppointmentsTab;
         }
 
-        NotificationCenter.Current.NotificationTapped += GotoAppointmentsTab;
-    }
-
-    private void GotoAppointmentsTab(NotificationEventArgs e)
-    {
-        if (_connectivity.NetworkAccess == NetworkAccess.Internet)
+        private void GotoAppointmentsTab(NotificationEventArgs e)
         {
-            _navigationService.SetRootPage<MainPage>(isOffWhitePageBgColor: true);
+            if (_connectivity.NetworkAccess == NetworkAccess.Internet)
+            {
+                _navigationService.SetRootPage<MainPage>(isOffWhitePageBgColor: true);
 
-            var bindingContext = (TabMainPageViewModelBase)_navigationService.GetCurrentPage().BindingContext;
-            bindingContext.ActiveTabIndex = 3;
-            foreach (var item in bindingContext.Tabs)
-                item.IsCurrentTab = false;
+                var bindingContext = (TabMainPageViewModelBase)_navigationService.GetCurrentPage().BindingContext;
+                bindingContext.ActiveTabIndex = 3;
+                foreach (var item in bindingContext.Tabs)
+                    item.IsCurrentTab = false;
 
-            bindingContext.Tabs[3].IsCurrentTab = true;
-            bindingContext.Tabs[3].Initialized(null);
+                bindingContext.Tabs[3].IsCurrentTab = true;
+                bindingContext.Tabs[3].Initialized(null);
+            }
         }
-    }
 
-    protected override async void OnStart()
-    {
-        await _mainThread.InvokeOnMainThreadAsync(() => MainPage.DisplayAlert("Created by the MedikTapp Team",
-            "Capstone Symposium Best Presentor\n\n" +
-            "With the supervision of Prof. Josephine M. Dela Isla under DICT 3-1 Batch 2021-2022\n\n" +
-            "Group Leader:\n" +
-            "Recreo, Ann Bernadette B.\n\n" +
-            "Developers:\n" +
-            "Lumaque, Jhericoh Janquill N. - Lead Developer\n" +
-            "Corpuz, Bithiah Pelaiah T.\n\n" +
-            "Group Members:\n" +
-            "Guisadio, Ana Beatriz A.\n" +
-            "Hernandez, Johannah Marie R.\n" +
-            "Corales, Mark Reinell S.\n" +
-            "Fernandez, Kathrina Bianca R.\n" +
-            "Serrano, Regina Lei C.",
-            "OK"));
-
-        if (_connectivity.NetworkAccess == NetworkAccess.Internet)
+        protected override async void OnStart()
         {
-            _notificationService.PromoNotificationsSubscription(true);
-            await _initializeDataService.Init().ConfigureAwait(false);
-            await Task.WhenAll
-            (
-               _medikTappService.Init(),
-               _graphicsService.PreloadImages(),
-               _appConfigService.Init()
-            ).ConfigureAwait(false);
-        }
-        else
-        {
-            _toast.Show("An active internet connection is required to use MedikTapp.");
-        }
+            await _mainThread.InvokeOnMainThreadAsync(() => MainPage.DisplayAlert("Created by the MedikTapp Team",
+                "Capstone Symposium Best Presentor\n\n" +
+                "With the supervision of Prof. Josephine M. Dela Isla under DICT 3-1 Batch 2021-2022\n\n" +
+                "Group Leader:\n" +
+                "Recreo, Ann Bernadette B.\n\n" +
+                "Developers:\n" +
+                "Lumaque, Jhericoh Janquill N. - Lead Developer\n" +
+                "Corpuz, Bithiah Pelaiah T.\n\n" +
+                "Group Members:\n" +
+                "Guisadio, Ana Beatriz A.\n" +
+                "Hernandez, Johannah Marie R.\n" +
+                "Corales, Mark Reinell S.\n" +
+                "Fernandez, Kathrina Bianca R.\n" +
+                "Serrano, Regina Lei C.",
+                "OK"));
 
-        //await _mainThread.InvokeOnMainThreadAsync(() => MainPage.DisplayAlert("Created by the MedikTapp Team",
-        //    "Capstone Symposium Best Presentor\n\n" +
-        //    "With the supervision of Prof. Josephine M. Dela Isla under DICT 3-1 Batch 2021-2022\n\n" +
-        //    "Group Leader:\n" +
-        //    "Recreo, Ann Bernadette B.\n\n" +
-        //    "Developers:\n" +
-        //    "Lumaque, Jhericoh Janquill N. - Lead Developer\n" +
-        //    "Corpuz, Bithiah Pelaiah T.\n\n" +
-        //    "Group Members:\n" +
-        //    "Guisadio, Ana Beatriz A.\n" +
-        //    "Hernandez, Johannah Marie R.\n" +
-        //    "Corales, Mark Reinell S.\n" +
-        //    "Fernandez, Kathrina Bianca R.\n" +
-        //    "Serrano, Regina Lei C.",
-        //    "OK"));
+            if (_connectivity.NetworkAccess == NetworkAccess.Internet)
+            {
+                _notificationService.PromoNotificationsSubscription(true);
+                await _initializeDataService.Init().ConfigureAwait(false);
+                await Task.WhenAll
+                (
+                   _medikTappService.Init(),
+                   _graphicsService.PreloadImages(),
+                   _appConfigService.Init()
+                ).ConfigureAwait(false);
+            }
+            else
+            {
+                _toast.Show("An active internet connection is required to use MedikTapp.");
+            }
+
+            //await _mainThread.InvokeOnMainThreadAsync(() => MainPage.DisplayAlert("Created by the MedikTapp Team",
+            //    "Capstone Symposium Best Presentor\n\n" +
+            //    "With the supervision of Prof. Josephine M. Dela Isla under DICT 3-1 Batch 2021-2022\n\n" +
+            //    "Group Leader:\n" +
+            //    "Recreo, Ann Bernadette B.\n\n" +
+            //    "Developers:\n" +
+            //    "Lumaque, Jhericoh Janquill N. - Lead Developer\n" +
+            //    "Corpuz, Bithiah Pelaiah T.\n\n" +
+            //    "Group Members:\n" +
+            //    "Guisadio, Ana Beatriz A.\n" +
+            //    "Hernandez, Johannah Marie R.\n" +
+            //    "Corales, Mark Reinell S.\n" +
+            //    "Fernandez, Kathrina Bianca R.\n" +
+            //    "Serrano, Regina Lei C.",
+            //    "OK"));
+        }
     }
 }
